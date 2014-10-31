3ffcffd669c1d117d41676ee0e87ea76d58b1d9d
prschulz <prschulz88@gmail.com>
2014-10-31 09:35:00 -0700

    portz

--- app.js
+++ app.js
@@ -96,6 +96,6 @@ app.get('/search', function (req, res) {
 
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
